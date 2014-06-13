f6acb3886f985952c924d544df989809aa836d3d
AlexDenisov <1101.debian@gmail.com>
2014-06-13 20:20:33 +0300

    Initial commit

---
+++ Sleipnir/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sleipnir
+//
+//  Created by AlexDenisov on 6/13/14.
+//  Copyright (c) 2014 railsware. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
