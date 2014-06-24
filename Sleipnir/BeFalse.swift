9af75650c26498b1c2759733dcc10080bf5a9520
Artur Termenji <atermenji@gmail.com>
2014-06-24 14:21:47 +0300

    BeFalse matcher

---
+++ Sleipnir/BeFalse.swift
@@ -0,0 +1,28 @@
+//
+//  BeFalse.swift
+//  Sleipnir
+//
+//  Created by Artur Termenji on 6/24/14.
+//  Copyright (c) 2014 railsware. All rights reserved.
+//
+
+import Foundation
+
+class BeFalse<T>: BaseMatcher<T> {
+    
+    init(expected: T) {
+        super.init(expected: expected)
+    }
+    
+    override func match(actual: T) -> Bool {
+        return !(actual as Bool)
+    }
+    
+    override func failureMessage() -> String {
+        return "FAIL TO BE FALSE"
+    }
+}
+
+func beFalse() -> BeFalse<Bool> {
+    return BeFalse(expected: false)
+}
