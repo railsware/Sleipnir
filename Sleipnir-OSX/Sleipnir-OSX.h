afe2850d63fc94a894e4d163a4282cdf5c4d7cbe
AlexDenisov <1101.debian@gmail.com>
2014-07-06 11:46:52 +0300

    OSX framework added

---
+++ Sleipnir-OSX/Sleipnir-OSX.h
@@ -0,0 +1,19 @@
+//
+//  Sleipnir-OSX.h
+//  Sleipnir-OSX
+//
+//  Created by AlexDenisov on 7/6/14.
+//  Copyright (c) 2014 railsware. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Sleipnir-OSX.
+FOUNDATION_EXPORT double Sleipnir_OSXVersionNumber;
+
+//! Project version string for Sleipnir-OSX.
+FOUNDATION_EXPORT const unsigned char Sleipnir_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sleipnir_OSX/PublicHeader.h>
+
+
