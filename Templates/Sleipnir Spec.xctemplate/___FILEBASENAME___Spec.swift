a5988b1387fe241436c7ddd0e838d91479321687
AlexDenisov <1101.debian@gmail.com>
2014-08-22 09:49:17 +0300

    File template added

---
+++ Templates/Sleipnir Spec.xctemplate/___FILEBASENAME___Spec.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILEBASENAME___Spec.swift
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+import Sleipnir
+
+class ___FILEBASENAME___Spec : SleipnirSpec 
+{
+    let spec : () = describe("___FILEBASENAME___") {
+
+    }
+}
+
